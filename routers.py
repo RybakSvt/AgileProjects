14c9e76aebe03ccbd599c6a391cae953fd1519e1
Vladislav <parieiko.vad@gmail.com>
2025-12-04 00:09:45 +0400

    Authentication && Authorisation

--- routers.py
+++ routers.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
@@ -6,4 +7,6 @@ urlpatterns = [
     path('tasks/', include('projects.urls.task')),
     path('projects/', include('projects.urls.project')),
     path('project_files/', include('projects.urls.project_file')),
+    path('auth/login/', TokenObtainPairView.as_view()),
+    path('token-refresh/', TokenRefreshView.as_view()),
 ]
